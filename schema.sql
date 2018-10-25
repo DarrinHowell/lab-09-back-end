9f1d5c7835ab848125dc40461df56e1141d75315
Darrin Howell <dbhowell09@gmail.com>
2018-10-25 12:04:53 -0700

    initialized database

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS location;
 DROP TABLE IF EXISTS weather;
--- DROP TABLE IF EXISTS yelp;
--- DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS yelp;
+DROP TABLE IF EXISTS movies;
 
 CREATE TABLE location(
     id SERIAL PRIMARY KEY,
@@ -17,3 +17,23 @@ CREATE TABLE weather(
     time VARCHAR(255),
     location_id INTEGER NOT NULL REFERENCES location(id)
 );
+
+CREATE TABLE yelp(
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255),
+    url VARCHAR(255),
+    price VARCHAR(255),
+    image_url VARCHAR(255),
+    rating NUMERIC(2,1)
+);
+
+CREATE TABLE movies(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    overview VARCHAR(255),
+    average_votes NUMERIC(2,1),
+    image_url VARCHAR(255),
+    popularity NUMERIC(4,3),
+    released_on VARCHAR(255)
+);
+
